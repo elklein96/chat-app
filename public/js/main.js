ed73e5c2d57e3bd8004ce0c3aa987a61a7ebbf60
elklein96 <elklein96@gmail.com>
2016-09-06 00:43:55 -0400

    cosmetic fixes

--- public/js/main.js
+++ public/js/main.js
@@ -9,8 +9,19 @@ $(function() {
     	}
     });
 
+    $(document).on("keypress", "#message-input", function(e) {
+    	if (e.which == 13) {
+	    	var msg = $('#message-input').val();
+	    	if (msg.length > 0) {
+	    		socket.emit('message', { content: msg });
+	    		$('#message-input').val('');
+	    	}
+	    }
+    });
+
     socket.on('message', function(data) {
     	$('#chat-wrapper').append('<p class="message">' + data.content + '</p><br>');
-    	console.log(data);
+    	$('#chat-wrapper')[0].scrollTop = $('#chat-wrapper')[0].scrollHeight;
+    	$('#send').blur();
     });
 });
